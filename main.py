002e8643cd87e18a40e1f50cfd26b90004274a76
Safi <safi.sharieff@gmail.com>
2022-09-29 20:34:05 +0400

    Adding imports to the source code

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import pandas_datareader as pdt
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
