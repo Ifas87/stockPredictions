1727f1bc819c0d357a480ec24d61c792c0974d53
Safi <safi.sharieff@gmail.com>
2022-09-30 13:46:17 +0400

    Added machine learning libraries and target company

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.python.keras.layers import Dense, Deopout, LSTM 
+from tensorflow.python.keras.models import Sequential
+
+TARGET = 'INFN'
+
 
 def main():
     pass
