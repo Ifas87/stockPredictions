ca1d785d839fe96057cd1d80ecd5193ff755ac05
Safi <safi.sharieff@gmail.com>
2022-10-07 19:56:57 +0400

    07-10-2022 Changes
    -Attempting to recreate the project byt utilising a neural network

---
+++ test.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+iris = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/'
+'master/iris.csv') 
+
+#print(iris)
+
+# one line split 
+train, validation, test = np.split(iris.sample(frac=1), [int(.6*len(iris)),
+int(.8*len(iris))])
+
+
+print( [train.columns[i] for i in range(train.shape[1]-1) ] )
+
+"""
+print([int(.6*len(iris)), int(.8*len(iris))])
+print(train)
+print(test)
+print(validation)
+"""
+
+# Assign the train split
+X_train = train[[train.columns[i] for i in range(train.shape[1]-1) ]]
+y_train = train[train.columns[-1]]
+# Assign the test split
+X_test = test[[test.columns[i] for i in range(train.shape[1]-1) ]]
+y_test = test[test.columns[-1]]
+# Assign the validation split
+X_val = validation[[validation.columns[i] for i in 
+                    range(validation.shape[1]-1) ]]
+y_val = validation[validation.columns[-1]]
