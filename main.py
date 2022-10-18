32704ee28df80695651c5720bb53eac2db3c561c
Safi <safi.sharieff@gmail.com>
2022-10-18 18:50:57 +0400

    18-10-2022 Changes \n Made the finishing touches to the project

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
     model.add(Dropout(0.2))
     model.add(Dense(units=1))
 
-    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae', 'acc'])
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['acc'])
     model.fit(training_data, resulting_data, epochs=25, batch_size=32)
 
     graphing_sample_start = dt.datetime(2019, 1, 1)
@@ -56,45 +56,42 @@ def main():
     model_inputs = model_inputs.reshape(-1, 1)
     model_inputs = mapper.transform(model_inputs)
 
-    # print(len(total_dataset), len(test_data))
-    # print(len(total_dataset) - len(test_data) - prediction_duration)
+    graphing_data = []
 
-    # graphing_data = []
-
-    # for x in range(prediction_duration, len(model_inputs)):
-    #     graphing_data.append(model_inputs[x-prediction_duration:x, 0])
+    for x in range(prediction_duration, len(model_inputs)):
+        graphing_data.append(model_inputs[x-prediction_duration:x, 0])
     
-    # graphing_data = np.array(graphing_data)
-    # graphing_data = np.reshape(graphing_data, (graphing_data.shape[0], graphing_data.shape[1], 1))
+    graphing_data = np.array(graphing_data)
+    graphing_data = np.reshape(graphing_data, (graphing_data.shape[0], graphing_data.shape[1], 1))
 
-    # predicts = model.predict(graphing_data)
-    # predicts = mapper.inverse_transform(predicts)
+    predicts = model.predict(graphing_data)
+    predicts = mapper.inverse_transform(predicts)
     
-    # plt.plot(actual_prices, color="orange", label="True")
-    # plt.plot(predicts, color="blue", label="Predicted")
-    # plt.title(f" {TARGET} test prediction")
-    # plt.xlabel('Time')
-    # plt.ylabel('NAT share price')
+    plt.plot(actual_prices, color="orange", label="True")
+    plt.plot(predicts, color="blue", label="Predicted")
+    plt.title(f" {TARGET} test prediction")
+    plt.xlabel('Time')
+    plt.ylabel('NAT share price')
 
-    # plt.legend()
-    # plt.show()
+    plt.legend()
+    plt.show()
 
 
-    # Current day predictions
+    rd_sample = [model_inputs[len(model_inputs) + 1 - prediction_duration:len(model_inputs+1), 0]]
+    rd_sample = np.array(rd_sample)
+    rd_sample = np.reshape(rd_sample, (rd_sample.shape[0], rd_sample.shape[1], 1))
 
-    # rd_sample = [model_inputs[len(model_inputs) + 1 - prediction_duration:len(model_inputs+1), 0]]
-    # rd_sample = np.array(rd_sample)
-    # rd_sample = np.reshape(rd_sample, (rd_sample.shape[0], rd_sample.shape[1], 1))
-
-    # final_val = model.predict(rd_sample)
-    # final_val = mapper.inverse_transform(final_val)
-    # print(final_val)
+    final_val = model.predict(rd_sample)
+    final_val = mapper.inverse_transform(final_val)
+    print(final_val)
 
     evaluation_data = np.split(training_data, [ int(0.6*len(training_data)) ])[1]
     evaluation_result_data = np.split(resulting_data, [ int(0.6*len(resulting_data)) ])[1]
 
     print(len(evaluation_data), len(evaluation_result_data))
 
+    results = model.evaluate(evaluation_data, evaluation_result_data, verbose=2, batch_size=128)
+    print("test loss, test acc:", results)
     
 
 
